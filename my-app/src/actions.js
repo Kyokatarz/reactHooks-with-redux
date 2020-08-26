97c9ac56a3a7de43165acc574e5755ef7ae507e7
Kyokatarz <51933605+Kyokatarz@users.noreply.github.com>
2020-08-26 17:52:57 +0300

    Add comments

--- my-app/src/actions.js
+++ my-app/src/actions.js
@@ -1,9 +1,11 @@
-export const SET = 'SET';
-export const ADD = 'ADD';
-export const DELETE = 'DELETE';
-export const MODIFY = 'MODIFY';
+//To do list actions
+export const SET = "SET";
+export const ADD = "ADD";
+export const DELETE = "DELETE";
+export const MODIFY = "MODIFY";
 
-export const SEND = 'SEND';
-export const RESPONSE = 'RESPONSE';
-export const ERROR = 'ERROR';
-export const CLEAR = 'CLEAR';
+//http state actions
+export const SEND = "SEND";
+export const RESPONSE = "RESPONSE";
+export const ERROR = "ERROR";
+export const CLEAR = "CLEAR";
