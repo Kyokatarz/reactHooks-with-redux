9f295854db4a873b95feb21ee1236790a742ebd3
Kyokatarz <51933605+Kyokatarz@users.noreply.github.com>
2020-08-28 01:16:46 +0300

    add redux devtools

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,6 +5,8 @@ import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import toDoListReducer from "./Reducers/toDoListReducer";
 import httpStateReducer from "./Reducers/httpStateReducer";
+import alertReducer from "./Reducers/alertReducer";
+import { devToolsEnhancer } from "redux-devtools-extension";
 
 import "./Css/index.css";
 import App from "./App";
@@ -12,9 +14,10 @@ import App from "./App";
 const rootReducer = combineReducers({
   tdl: toDoListReducer,
   http: httpStateReducer,
+  alert: alertReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, devToolsEnhancer());
 
 ReactDOM.render(
   <React.StrictMode>
