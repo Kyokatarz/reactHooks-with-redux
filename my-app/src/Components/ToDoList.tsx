ae44d18ab22c8a5c78eacd9ab43bfa1c97e90f1c
Kyokatarz <51933605+Kyokatarz@users.noreply.github.com>
2020-08-28 01:18:15 +0300

    Split into seperated components

--- my-app/src/Components/ToDoList.tsx
+++ my-app/src/Components/ToDoList.tsx
@@ -1,18 +1,15 @@
-import React, { useEffect } from "react";
+import React from "react";
 import InputBar from "./UI/InputBar";
 import ListContainer from "./ListContainer";
-import ErrorAndLoading from "./ErrorAndLoading";
+import AlertContainer from "./AlertContainer";
 
 import "../Css/ToDoList.css";
 
 const ToDoList: React.FC = () => {
-  useEffect(() => {
-    console.log("Rendering ToDoList.js");
-  });
   return (
     <>
       <h1>To do list</h1>
-      <ErrorAndLoading />
+      <AlertContainer />
       <InputBar />
       <ListContainer />
     </>
---
+++ my-app/src/Components/UI/Loading.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../Css/LoadingIcon.css";
+
+const Loading: React.FC = () => {
+  return (
+    <div>
+      <div className="loadingio-spinner-rolling-q4fibdzvved" id="loading-icon">
+        <div className="ldio-pl86sw1k0i9">
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Loading;
